55284d7d38683899e03a8a9b08ecf5c5ccf179f1
kshriishwarya <46679711+kshriishwarya@users.noreply.github.com>
2020-03-06 17:16:24 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,67 @@ const path = require("path");
 var webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+// const bundleExtractPlugin = new ExtractTextPlugin({
+//   filename: 'css/bundle.css',
+//   });
 module.exports = {
 
   entry: {
     header: './js/header.js',
-    banner:"./js/banner.js"
+   banner:"./js/banner.js"
     
   },
   output: {
     filename: '[name].js',
     path: __dirname + '/dist'
   },
+  module:{
+  rules: [
+    {
+      test: /\.hbs$/,
+      exclude: /node_modules/,
+      use:["handlebars-loader"]
+      
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+        "sass-loader"
+      ]
+    },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/
+      // use: {
+      //   loader: 'babel-loader',
+      //   options: {
+      //     presets: ['@babel/preset-env']
+      //   }
+      // }
+    }
+  
+  ],
+  },
+plugins: [ 
+  new MiniCssExtractPlugin({
+    filename: "style.css"
+  }),
+  new HtmlWebpackPlugin({
+    inject: false,
+    hash: true,
+    template: './index.html',
+    filename: 'index.html'
+  }),
+  new webpack.ProvidePlugin({
+    $: "jquery",  
+    jQuery: "jquery" 
+})
+]}
+
  
   
-  module: {
-    rules: [
-      {
-        test: /\.hbs$/,
-        use: [{
-          loader: "handlebars-loader",
-       
-        }]
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-
-  plugins:[
-    new webpack.ProvidePlugin({
-      $: "jquery",  
-      jQuery: "jquery" 
-  })
-]
- 
-};
